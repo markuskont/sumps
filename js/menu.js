9f417d29f6b1dbdb6296167bbb90e433f66cb014
Markus Kont <markuskont@gmail.com>
2015-12-30 23:37:49 +0200

    Playing around with generic HTML creation and CSS dropdown menus

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,4 @@
-define(['d3', 'elasticsearch', 'config'], function (d3, elasticsearch, config){
+define(['elasticsearch', 'config'], function (elasticsearch, config){
   var d3_button_binding = "context-button";
   var menu = document.createElement("NAV");
   var lv1 = document.createElement("UL");
@@ -22,8 +22,17 @@ define(['d3', 'elasticsearch', 'config'], function (d3, elasticsearch, config){
       lv2_list.appendChild(lv2_link);
       lv2.appendChild(lv2_list);
 
-      //var files = d.aggregations.count_by_file.buckets;
-      //console.log(files);
+      var files = d.count_by_file.buckets;
+      var lv3 = document.createElement("UL");
+      files.forEach(function(d){
+        var lv3_list = document.createElement("LI");
+        var lv3_link = document.createElement("A");
+        lv3_link.href = "#";
+        lv3_link.appendChild(document.createTextNode(d.key));
+        lv3_list.appendChild(lv3_link);
+        lv3.appendChild(lv3_list);
+      });
+      lv2_list.appendChild(lv3);
     });
 
     lv1_list.appendChild(lv2);
@@ -33,7 +42,4 @@ define(['d3', 'elasticsearch', 'config'], function (d3, elasticsearch, config){
   menu.appendChild(lv1);
   document.getElementById("menu").appendChild(menu);
 
-  // d3 testing
-  d3.selectAll("." + d3_button_binding)
-    .on('click' , function(d) { console.log("test " + d); return d; })
 });
