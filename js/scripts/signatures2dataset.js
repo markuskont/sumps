d9baf0b28348cc5a3882b8a9cc3ae1f1df5ada3f
Mauno <mauno.pihelgas@ccdcoe.org>
2016-01-13 13:13:27 +0200

    Added functionality for expanding variables within rules. Vars are read dynamically from Snort or Suricata conf.

--- js/scripts/signatures2dataset.js
+++ js/scripts/signatures2dataset.js
@@ -1,21 +1,60 @@
 var fs = require('fs');
 var http = require('http');
 var LineByLineReader = require('line-by-line');
+var yamlParser = require('yamljs')
 // var merge = require('merge');
 
 if (process.argv.length <= 2) {
-  console.log("Usage: " + __filename + " path/to/directory");
+  console.log("Usage: " + __filename + " path/to/directory [path/to/conffile]");
   process.exit(-1);
 }
 
 var path = process.argv[2];
+var expand = 0;
+var ipvars = [];
+var portvars = {};
+
+// Parse conf for variables
+if (process.argv[3]) {
+    expand = 1;
+    var file = process.argv[3];
+    
+    //If the file is not YAML (Suricata conf), then it will look for Snort specific keywords, or else it will fail.
+    var content = fs.readFileSync(file, 'utf8');
+    if (content.indexOf('%YAML') != -1) { // Suricata
+      var nativeObject = yamlParser.load(file);
+      ipvars = nativeObject['vars']['address-groups'];
+      portvars = nativeObject['vars']['port-groups'];
+    }
+    else if (content.indexOf('ipvar')){ // Snort
+      var pattern = new RegExp(/(ipvar|portvar) (\S+) (\S+)/gm);
+      var found = content.match(pattern);
+      
+      for (var i = 0; i < found.length; i++) {
+        var line = found[i];
+        if (line.indexOf('ipvar') != -1) {
+          var res = (line.substr(line.indexOf(' ')+1).split(' '));
+          ipvars[res[0]] = res[1];
+        } else {
+          var res = (line.substr(line.indexOf(' ')+1).split(' '));
+          portvars[res[0]] = res[1];
+        }
+      }
+    }
+    else { // bail out
+      console.err("Could not parse variables from the specified configuration file");
+      process.exit(-2);
+    }
+    //console.log(ipvars + portvars); //debug
+    
+}
 
 fs.readdir(path, function(err, items) {
   // console.log(items);
   for (var i=0; i<items.length; i++) {
     // console.log(items[i]);
     // var file = path + items[i];
-    if ( filenameEndsWith(items[i],'.rules'))
+    if ( filenameEndsWith(items[i],'\.rules'))
         // console.log(file);
       readFile(path,items[i])
   }
@@ -51,29 +90,6 @@ function getValue(string){
   }
 }
 
-/**
- * Take object and check type
- * Convert to array if string, else add value
- * String checking disabled for the time being
- * @param obj
- * @param value
- * @returns array with value added to it
- */
-/** 
-function addValue(obj,value) {
-  if (isArray(obj)) {
-    //arr = obj.push(value);
-    obj.push(value);
-    return obj;
-  } else {
-    //console.log('not array');
-    var arr = [];
-    arr.push(obj);
-    arr.push(value);
-    return arr;
-  }
-}
-*/
 
 /**
  * https://www.whatsnoodle.com/check-variable-is-array-in-javascript/
@@ -88,6 +104,32 @@ function isArray(check_var) {
  */
 function isString(check_var) {
     return(Object.prototype.toString.call( check_var ) === '[object String]');
+}
+
+function expandVars(type, check_var) {
+    if (expand == 0)
+        return check_var;
+      
+    //console.log("IPUT: " + check_var); //debug
+    check_var = check_var.toString();
+    var pattern = new RegExp(/\$([A-Za-z0-9_-]+)/g);
+    var found = check_var.match(pattern);
+    //console.log("FOUND: " + found); //debug
+
+    if (found) {
+      for (var i=0; i < found.length; i++) {
+        match = found[i];
+        if (type == 'addr') {
+                    check_var = check_var.replace(match, ipvars[match.substr(1)]);
+        } else {
+                    check_var = check_var.replace(match, portvars[match.substr(1)]);
+        }
+      }
+      // Check if the new value was perhaps still a variable containing a $
+      check_var = expandVars(type, check_var);
+    }
+    //console.log("OPUT: " + check_var); //debug
+    return check_var;
 }
 
 /**
@@ -175,25 +217,26 @@ function readFile(path,fileName) {
   var stream = new LineByLineReader(file);
   var regex = new RegExp('^(alert|pass|drop|sdrop|reject|log|activate|dynamic) (\\S+) (\\S+) (\\S+) ([<-]>) (\\S+) (\\S+) \\((.+)\\)');
 
-
   stream.on('line', function(line) {
     if (line.match(regex)!=null) {
 
       var opts = parseSequence(line.match(regex)[8]);
-
+            
       var signature = {
         "file": fileName,
         "action": line.match(regex)[1],
         "proto": line.match(regex)[2],
-        "src_addr": line.match(regex)[3],
-        "src_port": line.match(regex)[4],
-        "destination" : line.match(regex)[5],
-        "dst_addr": line.match(regex)[6],
-        "dst_port": line.match(regex)[7],
+        "src_addr": expandVars('addr', line.match(regex)[3]),
+        "src_port": expandVars('port', line.match(regex)[4]),
+        "destination": line.match(regex)[5],
+        "dst_addr": expandVars('addr', line.match(regex)[6]),
+        "dst_port": expandVars('port', line.match(regex)[7]),
         "parameters": opts
       }
+      
       // I assume that keys in both hashes are distinct
       // var merged = merge_options(signature, opts)
+      //console.log(line); //original rule debug
       console.log(JSON.stringify(signature));
     }
   });
